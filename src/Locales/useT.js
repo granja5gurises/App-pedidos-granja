a79696fa00a2709d026946caabd1e3c554d5f1d1
granja5gurises <wgallinger@gmail.com>
2025-06-17 01:06:17 -0300

    Delete src/Locales/useT.js

--- src/Locales/useT.js
+++
@@ -1,7 +0,0 @@
-// src/locales/useT.js
-
-// Hook de traducción dummy: devuelve la clave tal cual.
-// Al final lo cambiamos por uno que lea los archivos de idioma.
-export default function useT() {
-  return (clave, vars) => typeof clave === "string" ? clave : "";
-}
